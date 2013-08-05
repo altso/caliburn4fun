c9eb3eed6d5c5a3678a8883cd4df601f825cf430
Alexander Tsoi <aliaksandr_tsoi@epam.com>
2013-08-05 21:56:57 +0300

    Initial release.

---
+++ Caliburn.Micro.Coding4Fun/Coding4FunDialog.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using Coding4Fun.Toolkit.Controls;
+
+namespace Caliburn.Micro.Coding4Fun
+{
+    public class Coding4FunDialog : PopUp<object, PopUpResult>
+    {
+        public static readonly DependencyProperty RootModelProperty =
+            DependencyProperty.Register("RootModel", typeof(object), typeof(Coding4FunDialog), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ContextProperty =
+            DependencyProperty.Register("Context", typeof(object), typeof(Coding4FunDialog), new PropertyMetadata(null));
+
+        public object RootModel
+        {
+            get { return GetValue(RootModelProperty); }
+            set { SetValue(RootModelProperty, value); }
+        }
+
+        public object Context
+        {
+            get { return GetValue(ContextProperty); }
+            set { SetValue(ContextProperty, value); }
+        }
+
+        public Coding4FunDialog()
+        {
+            DefaultStyleKey = typeof(Coding4FunDialog);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var host = (Coding4FunDialogHost)GetTemplateChild("ViewContainer");
+            var view = ViewLocator.LocateForModel(RootModel, host, Context);
+            host.Content = view;
+            host.SetValue(View.IsGeneratedProperty, true);
+
+            ViewModelBinder.Bind(RootModel, host, null);
+            Action.SetTarget(host, RootModel);
+
+            host.Closed += (sender, args) => OnCompleted(new PopUpEventArgs<object, PopUpResult>
+            {
+                Result = PopUpResult.Ok
+            });
+        }
+    }
+}
---
+++ Caliburn.Micro.Coding4Fun/Coding4FunWindowManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Caliburn.Micro.Coding4Fun
+{
+    public class Coding4FunWindowManager : IWindowManager
+    {
+        public void ShowDialog(object rootModel, object context = null, IDictionary<string, object> settings = null)
+        {
+            var dialog = new Coding4FunDialog
+            {
+                Context = context,
+                RootModel = rootModel
+            };
+
+            var activate = rootModel as IActivate;
+            if (activate != null)
+            {
+                activate.Activate();
+            }
+
+            var deactivate = rootModel as IDeactivate;
+            if (deactivate != null)
+            {
+                dialog.Completed += (sender, args) => deactivate.Deactivate(true);
+            }
+
+            dialog.Show();
+        }
+
+        public void ShowPopup(object rootModel, object context = null, IDictionary<string, object> settings = null)
+        {
+            ShowDialog(rootModel, context, settings);
+        }
+    }
+}
